09afa9c09e2a8933048b8ded505feeecd9ea2b39
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-07-03 21:22:51 +0530

    Add project files.

---
+++ FinalMIVIProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FinalMIVIProject.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FinalMIVIProject.Areas.Identity.IdentityHostingStartup))]
+namespace FinalMIVIProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
